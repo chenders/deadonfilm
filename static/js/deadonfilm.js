38abb8179fd3da4c54834b56fc4ce12433b34034
Chris Henderson <chris@waitingforthefuture.org>
2015-03-04 01:14:52 -0500

    fix some ie bugs, use bootstrap better, clean up css, remove js-dependent centering stuff

--- static/js/deadonfilm.js
+++ static/js/deadonfilm.js
@@ -1,13 +1,3 @@
-$(window).resize(function () {
-    $('.vertical-center').css({
-        display: 'block',
-        position: 'absolute',
-        top: ($(window).height() - $('.vertical-center').outerHeight()) / 3
-    });
-    $('#dead-row').css({width: $('.input-group').width()});
-
-});
-
 var engine = new Bloodhound({
     name: 'movies',
     remote: '/search/?q=%QUERY',
@@ -28,9 +18,9 @@ $('.typeahead').typeahead({
         source: engine.ttAdapter()
     }
 ).on('typeahead:opened', function (obj, datum) {
-    $('.pasto-row').remove();
+    $('.dead-row').remove();
 }).on('typeahead:selected', function(obj, datum) {
-    $('.pasto-row').remove();
+    $('.dead-row').remove();
     $('#spinner').show();
     $.ajax({
         url: '/died/' + datum.id,
@@ -43,18 +33,22 @@ $('.typeahead').typeahead({
             if (Object.keys(data).length > 0) {
                 var pastos = '';
                 $.each(data, function (idx, el) {
-                    pastos += '<div class="row pasto-row">' +
-                    '<div class="pasto col-md-8">' + el.name + ' <span>(' + el.character + ')</span></div>' +
-                    '<div class="died col-md-4">' + el.death + '</div>' +
+                    pastos += '<div class="row dead-row">' +
+                    '<div class="pasto col-sm-offset-3 col-sm-4">' + el.name + ' <span>(' + el.character + ')</span></div>' +
+                    '<div class="died col-sm-2">' + el.death + '</div>' +
                     '</div>';
                 })
             } else {
-                pastos = '<div class="row"><div class="col-md-8">Everyone\'s still alive!</div></div>';
+                pastos = '<div class="row dead-row"><div class="col-sm-offset-3 col-sm-6">Everyone\'s still alive!</div></div>';
             }
-            $('#dead-row').html(pastos);
+            $('.container').append(pastos);
         }
     });
 });
-setTimeout(function () {
-    $(window).resize();
-}, 500);
+$(document).ready(function () {
+    // IE9 and below do not have native support. :(
+    if ($.fn.placeholder) {
+        $('input').placeholder();
+    }
+    $('input.typeahead').focus();
+});
