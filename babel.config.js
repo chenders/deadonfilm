c16f8816a49224079b97b232cb663be44c0b6463
Chris <chris@waitingforthefuture.org>
2021-10-24 00:19:02 -0700

    lint

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   const presets = [
     [
       "@babel/preset-env",
       {
         useBuiltIns: "usage",
-        corejs: 3
-      }
+        corejs: 3,
+      },
     ],
     "@babel/preset-react",
-    "@babel/preset-typescript"
+    "@babel/preset-typescript",
   ];
   const plugins = [
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-proposal-optional-chaining"
+    "@babel/plugin-proposal-optional-chaining",
   ];
   return {
     presets,
-    plugins
+    plugins,
   };
 };
