f1375e2dfd627062af2757be1cf8704ab6a06aa0
Chris <chris@waitingforthefuture.org>
2021-10-23 23:22:26 -0700

    corejs/browserlist upgrade

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ module.exports = function(api) {
     [
       "@babel/preset-env",
       {
-        useBuiltIns: "usage"
+        useBuiltIns: "usage",
+        corejs: 3
       }
     ],
     "@babel/preset-react",
